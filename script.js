81a51b319dff42971d5c96e4680095f7ae1f7727
esrere <enuksa3@edu.riga.lv>
2026-06-16 23:02:48 +0300

    Update script.js

--- script.js
+++ script.js
@@ -25,7 +25,7 @@ const categories = [
         100: ["Kas tā par meiteni blakus man? (+ punkti, ja nosauksiet vārdu)", "Emma", "Miemma.JPG"],
         200: ["", ""],
         300: ["Question", "Answer"],
-        400: ["Question", "Answer"],
+        400: ["Kādas sekas bija šim incidentam?", "Mākslasskolā brīvie skapīši tika aizslēgti", "Kruzite.mp4"],
         500: ["Question", "Answer"]
     }
 },
